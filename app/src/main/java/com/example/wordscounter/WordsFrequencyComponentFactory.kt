c9f04d3f0cea3454f8874bf852e10155d2ed3b3e
Maksim Sukhotski <suhockii@gmail.com>
2023-09-20 12:12:04 +0200

    factory api

--- app/src/main/java/com/example/wordscounter/WordsFrequencyComponentFactory.kt
+++ app/src/main/java/com/example/wordscounter/WordsFrequencyComponentFactory.kt
@@ -1,16 +1,16 @@
 package com.example.wordscounter
 
+import android.app.Activity
 import android.content.Intent
 import androidx.core.app.AppComponentFactory
 import com.example.wordscounter.di.scope.RootScope
-import com.example.wordscounter.ui.WordsFrequencyActivity
 
 class WordsFrequencyComponentFactory : AppComponentFactory() {
     override fun instantiateActivityCompat(
         cl: ClassLoader,
         className: String,
         intent: Intent?
-    ): WordsFrequencyActivity = with(RootScope.scopes.activity) {
+    ): Activity = with(RootScope.scopes.activity) {
         if (!isOpen()) {
             create()
         }
